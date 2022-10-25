2727ba7901f5f4c664d3edd86e3c3bd33d5e9a0f
grcampillos <83939773+grcampillos@users.noreply.github.com>
2022-10-25 22:03:49 +0800

    Add files via upload

---
+++ reg_validation.js
@@ -0,0 +1,48 @@
+function formValidation() {
+    var uid = document.registration.userid;
+    var passid = document.registration.passid;
+    var uname = document.registration.username;
+    var uadd = document.registration.address;
+    var ucountry = document.registration.country;
+    var uzip = document.registration.zip;
+    var uemail = document.registration.email;
+    var umsex = document.registration.sex;
+
+
+
+    
+function userid_validation(uid,mx,my)
+{
+    var uid_len = uid.value.length;
+    if (uid_len == 0 || uid_len >= my || uid_len < mx) {
+        alert("User Id should not be empty / length be between "+mx+" to "+my);
+        uid.focus();
+        return false;
+    }
+    return true;
+}
+
+
+function passid_validation(passid,mx,my)
+{
+    var passid_len = passid.value.length;
+    if (passid_len == 0 || passid_len >= my || passid_len < mx) {
+        alert("Password should not be empty / length be between "+mx+" to "+my);
+        passid.focus();
+        return false;
+    }
+    return true;
+}
+
+function allLetter(uname){ 
+var letters = /^[A-Za-z]+$/;
+    if(uname.value.match(letters)){
+        return true;
+    }
+    else{
+        alert('Username must have alphabet characters only');
+        uname.focus();
+        return false;
+    }
+}
+}
