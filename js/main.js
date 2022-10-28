fac048a58e4046d96b1115e6037cef6c5db92d6a
adionisiio <83940096+adionisiio@users.noreply.github.com>
2022-10-28 23:19:42 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,31 +1,34 @@
-function Validate(txt) {
-    txt.value = txt.value.replace(/[^a-zA-Z-'\n\r.]+/g, '');
+function validateName() {
+	var name = document.getElementsByName("fname")[0].value;
+	if (name.length < 2)
+		return "Name is required";	
+	return "";
 }
 
-function email_validate(pemail)
-{
-var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})$/;
-
-    if(regMail.test(email) == false)
-    {
-    document.getElementById("pemail").innerHTML    = "<span class='warning'>Email address is not valid yet.</span>";
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
-    else
-    {
-    document.getElementById("pemail").innerHTML	= "<span class='valid'>Thanks, you have entered a valid Email address!</span>";	
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function agreeTerms(){
+    var decider = document.getElementById('field_terms');
+    if(submit = true && decider.checked == true){
+        alert('Your registration has been successful. Thank you for joining us!')
+        
+    }else{
+        alert('Please indicate that you have read and agree to the Terms and Conditions.');
     }
 }
 
-function email_validate(ueemail)
-{
-var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})$/;
 
-    if(regMail.test(email) == false)
-    {
-    document.getElementById("ueemail").innerHTML    = "<span class='warning'>Email address is not valid yet.</span>";
-    }
-    else
-    {
-    document.getElementById("ueemail").innerHTML	= "<span class='valid'>Thanks, you have entered a valid Email address!</span>";	
-    }
-}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
